59854c5955720f1054ae8a5251dc36ba01dc6940
nyaaar-wsl <nyaaarlathotep@163.com>
2024-02-05 09:52:01 +0800

    feta day01part2

--- src/day01/mod.rs
+++ src/day01/mod.rs
@@ -1,3 +1,5 @@
+use std::collections::HashMap;
+
 pub fn part01(input: &str) -> i64 {
     let mut total: i64 = 0;
     let mut first: i64 = -1;
@@ -19,5 +21,56 @@ pub fn part01(input: &str) -> i64 {
         }
     }
     println!("count: {:?}", count);
+    return total;
+}
+
+pub fn part02(input: &str) -> i64 {
+    let mut number_map: HashMap<&str, i64> = HashMap::new();
+
+    number_map.insert("one", 1);
+    number_map.insert("two", 2);
+    number_map.insert("three", 3);
+    number_map.insert("four", 4);
+    number_map.insert("five", 5);
+    number_map.insert("six", 6);
+    number_map.insert("seven", 7);
+    number_map.insert("eight", 8);
+    number_map.insert("nine", 9);
+    number_map.insert("1", 1);
+    number_map.insert("2", 2);
+    number_map.insert("3", 3);
+    number_map.insert("4", 4);
+    number_map.insert("5", 5);
+    number_map.insert("6", 6);
+    number_map.insert("7", 7);
+    number_map.insert("8", 8);
+    number_map.insert("9", 9);
+    let total: i64 = input
+        .lines()
+        .filter_map(|line| {
+            let first = (0..line.len()).find_map(|start| {
+                number_map.iter().find_map(|(str, val)| {
+                    if line[start..].starts_with(str) {
+                        return Some(val);
+                    } else {
+                        return None;
+                    }
+                })
+            });
+            let last = (0..line.len()).rev().find_map(|end| {
+                number_map.iter().find_map(|(str, val)| {
+                    if line[..=end].ends_with(str) {
+                        return Some(val);
+                    } else {
+                        return None;
+                    }
+                })
+            });
+            first.zip(last).map(|(a, b)| {
+                return a * 10 + b;
+            })
+        })
+        .sum();
+    println!("{:?}", total);
     return total;
 }
--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,20 @@
 use std::fs;
 use std::time::Instant;
 
-use crate::day01::part01;
-
 pub mod day01;
+use crate::day01::part01;
+use crate::day01::part02;
 
 fn main() {
-    let res = read_file_string("./src/day01/input");
+    let _test = "./src/day01/test";
+    let _input = "./src/day01/input";
+    let res = read_file_string(_input);
+    // let res = read_file_string(_test);
     let start = Instant::now();
     match res {
         Ok(v) => {
             println!("part 1 res: {:?}", part01(v.as_str()));
+            println!("part 2 res: {:?}", part02(v.as_str()));
         }
         Err(e) => {
             println!("err:{e:?}")
