6ddfcf0823c06ebb0d4af290363f015f2a0e07e3
nyaaar-wsl <nyaaarlathotep@163.com>
2024-02-08 16:54:30 +0800

    day03part2

--- src/day03/mod.rs
+++ src/day03/mod.rs
@@ -1,4 +1,4 @@
-use std::char;
+use std::{char, collections::HashMap};
 
 pub fn part01(input: &str) -> i64 {
     let mut num_total_now: Option<u32> = None;
@@ -16,7 +16,7 @@ pub fn part01(input: &str) -> i64 {
                     index_start = Some(col);
                 }
             } else {
-                is_num =false;
+                is_num = false;
             }
             if !is_num || col == line.len() - 1 {
                 if let Some((i_start, num)) = index_start.zip(num_total_now) {
@@ -35,7 +35,12 @@ pub fn part01(input: &str) -> i64 {
     return total_res;
 }
 
-fn get_operation(row: usize, i_start: usize, end: usize, num_pic: &Vec<Vec<char>>) -> Option<char> {
+fn get_operation(
+    row: usize,
+    i_start: usize,
+    end: usize,
+    num_pic: &Vec<Vec<char>>,
+) -> Option<(char, usize, usize)> {
     for i in row.saturating_sub(1)..=row.saturating_add(1) {
         for j in i_start.saturating_sub(1)..=end.saturating_add(1) {
             if let Some(operation) = num_pic
@@ -44,7 +49,7 @@ fn get_operation(row: usize, i_start: usize, end: usize, num_pic: &Vec<Vec<char>
             {
                 if !operation.is_numeric() && *operation != ('.') {
                     // println!("find operation:{:?},{:?}-->{:?}", i, j, operation);
-                    return Some(*operation);
+                    return Some((*operation, i, j));
                 }
             }
         }
@@ -53,5 +58,50 @@ fn get_operation(row: usize, i_start: usize, end: usize, num_pic: &Vec<Vec<char>
 }
 
 pub fn part02(input: &str) -> i64 {
-    return -1;
+    let mut num_total_now: Option<u32> = None;
+    let mut index_start: Option<usize> = None;
+    let num_pic: Vec<Vec<char>> = input.lines().map(|line| line.chars().collect()).collect();
+    let mut gear_map: HashMap<(usize, usize), (bool, i64)> = HashMap::new();
+
+    for (row, line) in input.lines().enumerate() {
+        for (col, char) in line.chars().into_iter().enumerate() {
+            let is_num: bool;
+            if let Some(num) = char.to_digit(10) {
+                is_num = true;
+                num_total_now = Some(num_total_now.unwrap_or(0) * 10 + num);
+                if index_start.is_none() {
+                    index_start = Some(col);
+                }
+            } else {
+                is_num = false;
+            }
+            if !is_num || col == line.len() - 1 {
+                if let Some((i_start, num)) = index_start.zip(num_total_now) {
+                    let end = if is_num { col } else { col - 1 };
+                    // let operation = get_operation(row, i_start, end, &num_pic);
+                    if let Some(operation) = get_operation(row, i_start, end, &num_pic) {
+                        // println!("add:{:?}", num);
+                        let pos = (operation.1, operation.2);
+                        if let Some(part) = gear_map.get(&pos) {
+                            gear_map.insert(pos, (true, part.1 * num as i64));
+                        } else {
+                            gear_map.insert(pos, (false, num as i64));
+                        }
+                    }
+                }
+                num_total_now = None;
+                index_start = None;
+            }
+        }
+    }
+    return gear_map
+        .iter()
+        .map(|(k, v)| {
+            if v.0 {
+                // println!("gear pos:{} {}, sum:{}", k.0, k.1, v.1);
+                return v.1;
+            }
+            return 0;
+        })
+        .sum();
 }
