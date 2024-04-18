c0fca4849280aa9708a7a5c0531c7f3058bf4859
nyaaar-wsl <nyaaarlathotep@163.com>
2024-04-18 15:35:42 +0800

    day08 cycle

--- src/day08/mod.rs
+++ src/day08/mod.rs
@@ -1,9 +1,28 @@
-use std::{collections::HashMap, intrinsics::mir::Len, str::FromStr};
+use std::{collections::HashMap,  str::FromStr};
 
 pub fn part01(input: &str) -> i64 {
-    // input.split_once("\n\n").map(|(instract,map)|{
-
-    // })
+    let network = input.parse::<Network>().ok().unwrap();
+    println!("{:?}", network);
+    let mut steps = 0;
+    let mut pos_now = "AAA";
+    // let mut fimilar_scece: HashMap<usize, Vec<String>> = HashMap::new();
+    for c in network.instrcts.chars().cycle(){
+     if pos_now== "ZZZ" {
+            return steps as i64;
+        }
+        let next_pos=network.map.get(pos_now).unwrap();
+        match c {
+            'L' => {
+                
+            }
+            'R' => {}
+            _ => {
+                panic!("{:?}", c);
+            }
+        }
+        // pos_now=network.map.get(pos_now)
+        // fimilar_scece.get(&i).map(|vec| {});
+    }
     return 0;
 }
 
@@ -18,22 +37,22 @@ impl FromStr for Network {
     type Err = parseErr;
     fn from_str(s: &str) -> Result<Self, Self::Err> {
         let mut map: HashMap<String, (String, String)> = HashMap::new();
-        let mut ins;
+        let mut ins = "";
         s.split_once("\n\n").map(|(up, down)| {
             ins = up;
-            down.lines().map(|line| {
+            down.lines().for_each(|line| {
                 line.split_once(" = ").map(|(start, end)| {
                     end.trim_matches(|char| char == '(' || char == ')')
                         .split_once(", ")
                         .map(|(l, r)| {
                             map.insert(start.to_string(), (l.to_string(), r.to_string()));
                         });
-                })
+                });
             });
         });
         Ok(Self {
             instrcts: ins.to_string(),
-            map: map,
+            map,
         })
     }
 }
