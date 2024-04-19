7a81547e5f756016ffcb90db87496a1b3f8b87d8
nyaaar-wsl <nyaaarlathotep@163.com>
2024-04-19 15:02:03 +0800

    day09part2

--- src/day09/mod.rs
+++ src/day09/mod.rs
@@ -1,5 +1,5 @@
 pub fn part01(input: &str) -> i64 {
-    let a = input
+    input
         .lines()
         .into_iter()
         .map(|l| {
@@ -13,8 +13,7 @@ pub fn part01(input: &str) -> i64 {
             let s = get_next(nums);
             return s;
         })
-        .sum::<i64>();
-    return a;
+        .sum::<i64>()
 }
 
 fn get_next(nums: Vec<i64>) -> i64 {
@@ -37,5 +36,38 @@ fn get_next(nums: Vec<i64>) -> i64 {
 }
 
 pub fn part02(input: &str) -> i64 {
-    return 0;
+    input
+        .lines()
+        .into_iter()
+        .map(|l| {
+            let nums: Vec<_> = l
+                .split(" ")
+                .into_iter()
+                .map(|raw_num| {
+                    return (raw_num.parse::<i64>()).ok().unwrap();
+                })
+                .collect();
+            let s = get_before(nums);
+            return s;
+        })
+        .sum::<i64>()
+}
+
+fn get_before(nums: Vec<i64>) -> i64 {
+    let mut all_zero = true;
+    for num in &nums {
+        if *num != 0 {
+            all_zero = false;
+            break;
+        }
+    }
+    if all_zero {
+        return 0;
+    }
+    let mut next_row: Vec<i64> = Vec::new();
+    for i in 0..(nums.len() - 1) {
+        next_row.push(nums[i + 1] - nums[i]);
+    }
+
+    nums[0] - get_before(next_row)
 }
