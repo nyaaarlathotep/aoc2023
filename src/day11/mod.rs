40bb7fd75289e77926ad25d99f2d344dbb6a9543
nyaaar-wsl <nyaaarlathotep@163.com>
2024-05-09 17:09:59 +0800

    day11part2 plus 99 times...

--- src/day11/mod.rs
+++ src/day11/mod.rs
@@ -1,6 +1,32 @@
 use std::cmp;
 
 pub fn part01(input: &str) -> i64 {
+    let (no_galaxy_rows, no_galaxy_cols, points) = parse_universe(input);
+    let mut res = 0;
+
+    for i in 0..points.len() {
+        for j in i..points.len() {
+            let p1 = points.get(i).unwrap();
+            let p2 = points.get(j).unwrap();
+
+            let extra_moves = get_extra_moves(p1, p2, &no_galaxy_rows, &no_galaxy_cols) as i64;
+            let normal_moves =
+                (p1.0 as i64 - p2.0 as i64).abs() + (p1.1 as i64 - p2.1 as i64).abs();
+            // println!(
+            //     "{:?}-{:?} extra:{:?},total:{:?}",
+            //     p1,
+            //     p2,
+            //     extra_moves,
+            //     extra_moves + normal_moves
+            // );
+            res = res + extra_moves + normal_moves;
+        }
+    }
+
+    return res;
+}
+
+fn parse_universe(input: &str) -> (Vec<usize>, Vec<usize>, Vec<(usize, usize)>) {
     let mut cosmic: Vec<Vec<char>> = Vec::with_capacity(12);
     let mut no_galaxy_rows: Vec<usize> = Vec::new();
     let mut no_galaxy_cols: Vec<usize> = Vec::new();
@@ -32,28 +58,7 @@ pub fn part01(input: &str) -> i64 {
             no_galaxy_cols.push(col);
         }
     }
-    let mut res = 0;
-
-    for i in 0..points.len() {
-        for j in i..points.len() {
-            let p1 = points.get(i).unwrap();
-            let p2 = points.get(j).unwrap();
-
-            let extra_moves = get_extra_moves(p1, p2, &no_galaxy_rows, &no_galaxy_cols) as i64;
-            let normal_moves =
-                (p1.0 as i64 - p2.0 as i64).abs() + (p1.1 as i64 - p2.1 as i64).abs();
-            // println!(
-            //     "{:?}-{:?} extra:{:?},total:{:?}",
-            //     p1,
-            //     p2,
-            //     extra_moves,
-            //     extra_moves + normal_moves
-            // );
-            res = res + extra_moves + normal_moves;
-        }
-    }
-
-    return res;
+    (no_galaxy_rows, no_galaxy_cols, points)
 }
 
 fn get_extra_moves(
@@ -81,5 +86,27 @@ fn get_extra_moves(
 }
 
 pub fn part02(input: &str) -> i64 {
-    return 0;
+    let (no_galaxy_rows, no_galaxy_cols, points) = parse_universe(input);
+    let mut res = 0;
+
+    for i in 0..points.len() {
+        for j in i+1..points.len() {
+            let p1 = points.get(i).unwrap();
+            let p2 = points.get(j).unwrap();
+
+            let extra_moves = (get_extra_moves(p1, p2, &no_galaxy_rows, &no_galaxy_cols) *999999 ) as i64;
+            let normal_moves =
+                (p1.0 as i64 - p2.0 as i64).abs() + (p1.1 as i64 - p2.1 as i64).abs();
+            // println!(
+            //     "{:?}-{:?} extra:{:?},total:{:?}",
+            //     p1,
+            //     p2,
+            //     extra_moves,
+            //     extra_moves + normal_moves
+            // );
+            res = res + extra_moves + normal_moves;
+        }
+    }
+
+    return res;
 }
