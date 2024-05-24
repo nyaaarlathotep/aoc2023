a45de2ac1367902408e81ea56e83be32beeb1fcb
nyaaar-wsl <nyaaarlathotep@163.com>
2024-05-24 13:27:38 +0800

    day13part1 fool.

--- src/day13/mod.rs
+++ src/day13/mod.rs
@@ -6,26 +6,25 @@ pub fn part01(input: &str) -> i64 {
         .map(|part| {
             let mirror: Vec<&[u8]> = part.lines().map(|l| l.as_bytes()).collect::<Vec<&[u8]>>();
 
-            let mirror_width = mirror.len();
-            let mirror_height = mirror[0].len();
+            let mirror_width = mirror[0].len();
+            let mirror_height = mirror.len();
             for i in 0..mirror_width - 1 {
                 let mut mirroralbe = true;
+                let max_mirror_len = min(i + 1, mirror_width - i - 1);
+                // println!("i + 1:{}", i + 1);
+                // println!("mirror_width - i - 1:{}", mirror_width - i - 1);
+                // println!("maxLen:{}", max_mirror_len);
                 for j in 0..mirror_height {
                     if !mirroralbe {
                         break;
                     }
-                    for len in 1..i {
-                        println!("{:?}-{:?}:{:?}", j, i-len+1, mirror.get(j).expect("?")[i] as char);
-                        println!(
-                            "{:?}-{:?}:{:?}",
-                            j,
-                            i + len,
-                            mirror.get(j).expect("?")[i + len] as char
-                        );
-                        println!("");
-                        if mirror.get(j).expect("?")[i - len + 1]
-                            != mirror.get(j).expect("???")[i + len]
-                        {
+                    for len in 0..max_mirror_len {
+                        let left_char = mirror.get(j).expect("?")[i - len];
+                        let right_char = mirror.get(j).expect("???")[i + 1 + len];
+                        // println!("{:?}-{:?}:{:?}", j, i - len, left_char);
+                        // println!("{:?}-{:?}:{:?}", j, i + 1 + len, right_char);
+                        // println!("");
+                        if left_char != right_char {
                             mirroralbe = false;
                             break;
                         }
@@ -36,28 +35,20 @@ pub fn part01(input: &str) -> i64 {
                 }
             }
             for i in 0..mirror_height - 1 {
+                let max_mirror_len = min(i + 1, mirror_height - i - 1);
                 let mut mirroralbe = true;
+                // println!("maxLen:{}", max_mirror_len);
                 for j in 0..mirror_width {
                     if !mirroralbe {
                         break;
                     }
-                    for len in 1..i {
-                        println!(
-                            "{:?}-{:?}:{:?}",
-                            i - len + 1,
-                            j,
-                            mirror.get(i - len + 1).expect("?")[j] as char
-                        );
-                        println!(
-                            "{:?}-{:?}:{:?}",
-                            i + len,
-                            j,
-                            mirror.get(i + len).expect("?")[j] as char
-                        );
-                        println!("");
-                        if mirror.get(i - len + 1).expect("?")[j]
-                            != mirror.get(i + len).expect("???")[j]
-                        {
+                    for len in 0..max_mirror_len {
+                        let left_num = mirror.get(i - len).expect("?")[j];
+                        let right_num = mirror.get(i + len + 1).expect("???")[j];
+                        // println!("{:?}-{:?}:{:?}", i - len, j, left_num);
+                        // println!("{:?}-{:?}:{:?}", i + 1 + len, j, right_num);
+                        // println!("");
+                        if left_num != right_num {
                             mirroralbe = false;
                             break;
                         }
