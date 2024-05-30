549adea0009dab41310e0440ed20ab7923c4ccad
nyaaar-wsl <nyaaarlathotep@163.com>
2024-05-30 10:39:03 +0800

    day16

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,11 @@
 use std::fs;
 use std::time::Instant;
 
-pub mod day15;
-use crate::day15::part01;
-use crate::day15::part02;
+pub mod day16;
+use crate::day16::part01;
+use crate::day16::part02;
 
-const DAY: &str = "15";
+const DAY: &str = "16";
 fn main() {
     let input = format!("./src/day{}/input", DAY);
     let res = read_file_string(input.as_str());
