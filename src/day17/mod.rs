0a0ef4ce6cd041d444015bd20b0297a08af334e8
nyaaar-wsl <nyaaarlathotep@163.com>
2024-06-07 15:34:01 +0800

    day17 wrong... I'd change a lot...

--- src/day17/mod.rs
+++ src/day17/mod.rs
@@ -1,3 +1,4 @@
+use core::panic;
 use std::collections::{hash_map::Entry, HashMap};
 
 pub fn part01(input: &str) -> Option<i64> {
@@ -21,13 +22,21 @@ pub fn part01(input: &str) -> Option<i64> {
         points,
     };
     grid.walk(Path {
-        steps: todo!(),
-        i: todo!(),
-        j: todo!(),
-        dir: todo!(),
-        limit: todo!(),
+        total_loss: 0,
+        i: 0,
+        j: 0,
+        dir: Direction::East,
+        limit: 3,
     });
-    return Some(0);
+    let min_move = grid.points[grid.height - 1][grid.width - 1]
+        .least_moves
+        .iter()
+        .map(|m| {
+            return m.1;
+        })
+        .min()
+        .unwrap();
+    return Some(*min_move as i64);
 }
 
 pub fn part02(input: &str) -> Option<i64> {
@@ -54,7 +63,7 @@ struct Point {
 
 #[derive(Debug, Copy, Clone, PartialEq, Eq, Hash)]
 struct Path {
-    steps: usize,
+    total_loss: usize,
     i: usize,
     j: usize,
     dir: Direction,
@@ -62,144 +71,135 @@ struct Path {
 }
 
 impl Grid {
-    pub fn walk(&mut self, mut path: Path) {
+    pub fn walk(&mut self, path: Path) {
         if path.i == self.height - 1 && path.j == self.width - 1 {
             self.points[path.i][path.j]
                 .least_moves
                 .entry((path.dir, path.limit))
                 .and_modify(|num| {
-                    if path.steps < *num {
-                        *num = path.steps;
+                    if path.total_loss < *num {
+                        *num = path.total_loss;
                     }
                 })
-                .or_insert(path.steps);
+                .or_insert(path.total_loss);
             return;
         }
         self.points[path.i][path.j]
             .least_moves
             .entry((path.dir, path.limit))
             .and_modify(|num| {
-                if path.steps < *num {
-                    *num = path.steps;
+                if path.total_loss < *num {
+                    *num = path.total_loss;
                 } else {
                     return;
                 }
             })
-            .or_insert(path.steps);
-        let mut next_path: Vec<Path> = Vec::new();
+            .or_insert(path.total_loss);
+        let mut next_paths: Vec<Direction> = Vec::new();
         match path.dir {
             Direction::North => {
                 if path.limit > 0 {
                     let n = opt_north(path);
                     if let Some(north) = n {
-                        next_path.push(north);
+                        next_paths.push(north);
                     }
                 }
                 let e = opt_east(path, self.width);
                 if let Some(east) = e {
-                    next_path.push(east);
+                    next_paths.push(east);
                 }
                 let w = opt_west(path);
                 if let Some(west) = w {
-                    next_path.push(west);
+                    next_paths.push(west);
                 }
             }
             Direction::South => {
                 if path.limit > 0 {
                     let s = opt_south(path, self.height);
                     if let Some(south) = s {
-                        next_path.push(south);
+                        next_paths.push(south);
                     }
                 }
                 let e = opt_east(path, self.width);
                 if let Some(east) = e {
-                    next_path.push(east);
+                    next_paths.push(east);
                 }
                 let w = opt_west(path);
                 if let Some(west) = w {
-                    next_path.push(west);
+                    next_paths.push(west);
                 }
             }
             Direction::East => {
                 if path.limit > 0 {
                     let e = opt_east(path, self.width);
                     if let Some(east) = e {
-                        next_path.push(east);
+                        next_paths.push(east);
                     }
                 }
                 let n = opt_north(path);
                 if let Some(north) = n {
-                    next_path.push(north);
+                    next_paths.push(north);
                 }
                 let s = opt_south(path, self.height);
                 if let Some(south) = s {
-                    next_path.push(south);
+                    next_paths.push(south);
                 }
             }
             Direction::West => {
                 if path.limit > 0 {
                     let w = opt_west(path);
                     if let Some(west) = w {
-                        next_path.push(west);
+                        next_paths.push(west);
                     }
                 }
                 let n = opt_north(path);
                 if let Some(north) = n {
-                    next_path.push(north);
+                    next_paths.push(north);
                 }
                 let s = opt_south(path, self.height);
                 if let Some(south) = s {
-                    next_path.push(south);
+                    next_paths.push(south);
                 }
             }
         }
-        next_path.into_iter().for_each(|p| {
-            self.walk(p);
+        next_paths.into_iter().for_each(|p| {
+            let mut next_path = path.clone();
+            next_path.dir = p;
+            next_path.total_loss += self.points[path.i][path.j].loss;
+            if p != path.dir {
+                next_path.limit = 4;
+            }
+            if next_path.limit == 0 {
+                panic!("turn err!");
+            }
+            next_path.limit -= 1;
+
+            self.walk(next_path);
         })
     }
 }
 
-fn opt_north(path: Path) -> Option<Path> {
-    let mut s = None;
+fn opt_north(path: Path) -> Option<Direction> {
     if path.i > 0 {
-        let mut north = path.clone();
-        north.limit -= 1;
-        north.steps = north.steps + 1;
-        north.i = north.i - 1;
-        s = Some(north);
+        return Some(Direction::North);
     }
-    s
+    None
 }
-fn opt_south(path: Path, height: usize) -> Option<Path> {
-    let mut s = None;
+fn opt_south(path: Path, height: usize) -> Option<Direction> {
     if path.i < height - 1 {
-        let mut north = path.clone();
-        north.limit -= 1;
-        north.steps = north.steps + 1;
-        north.i = north.i + 1;
-        s = Some(north);
+        return Some(Direction::South);
     }
-    s
+    None
 }
-fn opt_west(path: Path) -> Option<Path> {
-    let mut s = None;
-    if path.i > 0 {
-        let mut north = path.clone();
-        north.limit -= 1;
-        north.steps = north.steps + 1;
-        north.i = north.j - 1;
-        s = Some(north);
+fn opt_west(path: Path) -> Option<Direction> {
+    if path.j > 0 {
+        return Some(Direction::West);
     }
-    s
+    None
 }
-fn opt_east(path: Path, width: usize) -> Option<Path> {
-    let mut s = None;
+fn opt_east(path: Path, width: usize) -> Option<Direction> {
     if path.i < width - 1 {
-        let mut north = path.clone();
-        north.limit -= 1;
-        north.steps = north.steps + 1;
-        north.i = north.j + 1;
-        s = Some(north);
+        return Some(Direction::East);
     }
-    s
+    None
 }
