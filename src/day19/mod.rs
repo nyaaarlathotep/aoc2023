b1cc5e5d3bac4440fed5914edd025e26f10acfab
nyaaar-debian <nyaaarltahotep@163.com>
2024-06-16 21:31:38 +0800

    day19part1

--- src/day19/mod.rs
+++ src/day19/mod.rs
@@ -1,36 +1,59 @@
-use std::collections::HashMap;
+use std::{collections::HashMap, hash::Hash};
 
 use regex::Regex;
 
 pub fn part01(input: &str) -> Option<i64> {
     input.split_once("\n\n").map(|(rules, parts)| {
-        let rule_map: HashMap<&str, Vec<fn(Part) -> &str>> = HashMap::new();
+        let mut rule_map: HashMap<String, Vec<Rule>> = HashMap::new();
         let regex = Regex::new(r"(?m)^(?<name>[\w+]+)\{(?<cons>.*)\}").unwrap();
         regex.captures_iter(rules).for_each(|cap| {
-            println!("{:?}", &cap["name"]);
-            println!("{:?}", &cap["cons"]);
-            // let rules: Vec<fn(Part) -> &str> = &cap["cons"]
-            let rules = &cap["cons"]
+            // println!("{:?}", &cap["name"]);
+            // println!("{:?}", &cap["cons"]);
+            let rules = cap["cons"]
                 .split(",")
                 .map(|rule| {
-                    let r = Rule {
-                        f: Box::new(|part| {
-                            return rule;
-                        }),
-                    };
-                    if !rule.contains(":") {
+                    if rule.contains(":") {
+                        let (a, b) = rule.split_once(":").expect("!");
+                        let cmp = if rule.contains(">") { Cmp::gt } else { Cmp::lt };
+                        let (l, r) = match cmp {
+                            Cmp::gt => a.split_once(">").expect(">"),
+                            Cmp::lt => a.split_once("<").expect("<"),
+                        };
                         let r = Rule {
-                            f: Box::new(|_part| {
-                                return rule;
-                            }),
+                            direct: true,
+                            name: l.to_string(),
+                            cmp,
+                            value: r.parse::<usize>().expect("usize"),
+                            res: b.to_string(),
                         };
                         return r;
                     }
+                    let r = Rule {
+                        direct: false,
+                        name: "".to_string(),
+                        cmp: Cmp::gt,
+                        value: 1,
+                        res: "".to_string(),
+                    };
                     return r;
                 })
                 .collect::<Vec<Rule>>();
-        })
+            rule_map.insert(cap["name"].to_string(), rules);
+        });
+        let regex2 = Regex::new(r"(?m)^\{(?<part>.*)\}").unwrap();
+        let mut res = 0;
+        regex2.captures_iter(parts).for_each(|part| {
+            let a: HashMap<&str, usize> = part["part"]
+                .split(",")
+                .map(|p| {
+                    let a = p.split_once("=").unwrap();
+                    return (a.0, a.1.parse::<usize>().expect("????"));
+                })
+                .collect::<HashMap<&str, usize>>();
+            println!("{:?}", &a);
+        });
     });
+
     return Some(0);
 }
 
@@ -39,12 +62,21 @@ pub fn part02(input: &str) -> Option<i64> {
 }
 
 struct Rule {
-    f: Box<dyn Fn(Part) -> &str>,
+    direct: bool,
+    name: String,
+    cmp: Cmp,
+    value: usize,
+    res: String,
 }
 
-struct Part<'a> {
-    x: &'a str,
-    m: &'a str,
-    a: &'a str,
-    s: &'a str,
+enum Cmp {
+    gt,
+    lt,
+}
+
+struct Part {
+    x: usize,
+    m: usize,
+    a: usize,
+    s: usize,
 }
