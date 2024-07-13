e098c02de3da3bb7c594b843ed887df21714bf42
nyaaar-debian <nyaaarltahotep@163.com>
2024-07-13 17:00:46 +0800

    why

--- src/day23/mod.rs
+++ src/day23/mod.rs
@@ -1,3 +1,5 @@
+use itertools::Itertools;
+
 const ROCK: u8 = b'#';
 const PATH: u8 = b'.';
 const VISITED: u8 = b'O';
@@ -83,18 +85,29 @@ fn get_legal_neighbors(s: &Coordinate, map: &Vec<Vec<u8>>) -> Vec<(Coordinate, u
     }
     neighbors
 }
+
+struct MapNode {
+    mark: u8,
+    record: usize,
+}
 pub fn part02(input: &str) -> Option<i64> {
-    let mut map: Vec<Vec<u8>> = input
+    let mut map: Vec<Vec<MapNode>> = input
         .lines()
         .map(|l| {
             let a: String = String::from(l);
-            let v = a.into_bytes();
+            let v = a
+                .into_bytes()
+                .into_iter()
+                .map(|u| {
+                    return MapNode { mark: u, record: 0 };
+                })
+                .collect_vec();
             return v;
         })
         .collect();
     let mut start = Option::None;
     for i in 0..(&map[0]).len() {
-        if *&map[0][i] == b'.' {
+        if *&map[0][i].mark == b'.' {
             start = Some(Coordinate::new(0, i));
             break;
         }
@@ -102,13 +115,22 @@ pub fn part02(input: &str) -> Option<i64> {
     let s = start.expect("!");
     let mut res: Vec<usize> = Vec::new();
 
-    map[s.x][s.y] = VISITED;
+    map[s.x][s.y].mark = VISITED;
     walk_part2(&mut res, &mut map, 0, &s);
 
     return Some(res.into_iter().max().unwrap() as i64);
 }
 
-fn walk_part2(end_steps: &mut Vec<usize>, map: &mut Vec<Vec<u8>>, steps: usize, pos: &Coordinate) {
+fn walk_part2(
+    end_steps: &mut Vec<usize>,
+    map: &mut Vec<Vec<MapNode>>,
+    steps: usize,
+    pos: &Coordinate,
+) {
+    if steps < map[pos.x][pos.y].record {
+        return;
+    }
+    map[pos.x][pos.y].record = steps;
     if pos.x == map.len() - 1 {
         end_steps.push(steps);
         // print_map(&map);
@@ -116,36 +138,37 @@ fn walk_part2(end_steps: &mut Vec<usize>, map: &mut Vec<Vec<u8>>, steps: usize, 
     }
     let neighbors = get_legal_neighbors_part2(pos, map);
     for neighbor in neighbors {
-        map[neighbor.0.x][neighbor.0.y] = VISITED;
+        map[neighbor.0.x][neighbor.0.y].mark = VISITED;
         walk_part2(end_steps, map, steps + neighbor.1, &neighbor.0);
-        map[neighbor.0.x][neighbor.0.y] = PATH;
+        map[neighbor.0.x][neighbor.0.y].mark = PATH;
     }
 }
-fn get_legal_neighbors_part2(s: &Coordinate, map: &Vec<Vec<u8>>) -> Vec<(Coordinate, usize)> {
+fn get_legal_neighbors_part2(s: &Coordinate, map: &Vec<Vec<MapNode>>) -> Vec<(Coordinate, usize)> {
     let mut neighbors = Vec::new();
-    if s.x > 0 && map[s.x - 1][s.y] != ROCK && map[s.x - 1][s.y] != VISITED {
-        if map[s.x - 1][s.y] == b'^' {
+    if s.x > 0 && map[s.x - 1][s.y].mark != ROCK && map[s.x - 1][s.y].mark != VISITED {
+        if map[s.x - 1][s.y].mark == b'^' {
             neighbors.push((Coordinate::new(s.x - 2, s.y), 2));
-        } else  {
+        } else {
             neighbors.push((Coordinate::new(s.x - 1, s.y), 1));
         }
     }
-    if s.x + 1 < map.len() && map[s.x + 1][s.y] != ROCK && map[s.x + 1][s.y] != VISITED {
-        if map[s.x + 1][s.y] == b'v' {
+    if s.x + 1 < map.len() && map[s.x + 1][s.y].mark != ROCK && map[s.x + 1][s.y].mark != VISITED {
+        if map[s.x + 1][s.y].mark == b'v' {
             neighbors.push((Coordinate::new(s.x + 2, s.y), 2));
         } else {
             neighbors.push((Coordinate::new(s.x + 1, s.y), 1));
         }
     }
-    if s.y > 0 && map[s.x][s.y - 1] != ROCK && map[s.x][s.y - 1] != VISITED {
-        if map[s.x][s.y - 1] == b'<' {
+    if s.y > 0 && map[s.x][s.y - 1].mark != ROCK && map[s.x][s.y - 1].mark != VISITED {
+        if map[s.x][s.y - 1].mark == b'<' {
             neighbors.push((Coordinate::new(s.x, s.y - 2), 2));
-        } else  {
+        } else {
             neighbors.push((Coordinate::new(s.x, s.y - 1), 1));
         }
     }
-    if s.y + 1 < map[0].len() && map[s.x][s.y + 1] != ROCK && map[s.x][s.y + 1] != VISITED {
-        if map[s.x][s.y + 1] == b'>' {
+    if s.y + 1 < map[0].len() && map[s.x][s.y + 1].mark != ROCK && map[s.x][s.y + 1].mark != VISITED
+    {
+        if map[s.x][s.y + 1].mark == b'>' {
             neighbors.push((Coordinate::new(s.x, s.y + 2), 2));
         } else {
             neighbors.push((Coordinate::new(s.x, s.y + 1), 1));
