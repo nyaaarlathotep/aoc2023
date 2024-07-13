4047f68f64db4edd480725577bea8cd01bfec163
nyaaar-debian <nyaaarltahotep@163.com>
2024-07-13 16:46:36 +0800

    too slow

--- src/day23/mod.rs
+++ src/day23/mod.rs
@@ -84,7 +84,74 @@ fn get_legal_neighbors(s: &Coordinate, map: &Vec<Vec<u8>>) -> Vec<(Coordinate, u
     neighbors
 }
 pub fn part02(input: &str) -> Option<i64> {
-    return Some(0);
+    let mut map: Vec<Vec<u8>> = input
+        .lines()
+        .map(|l| {
+            let a: String = String::from(l);
+            let v = a.into_bytes();
+            return v;
+        })
+        .collect();
+    let mut start = Option::None;
+    for i in 0..(&map[0]).len() {
+        if *&map[0][i] == b'.' {
+            start = Some(Coordinate::new(0, i));
+            break;
+        }
+    }
+    let s = start.expect("!");
+    let mut res: Vec<usize> = Vec::new();
+
+    map[s.x][s.y] = VISITED;
+    walk_part2(&mut res, &mut map, 0, &s);
+
+    return Some(res.into_iter().max().unwrap() as i64);
+}
+
+fn walk_part2(end_steps: &mut Vec<usize>, map: &mut Vec<Vec<u8>>, steps: usize, pos: &Coordinate) {
+    if pos.x == map.len() - 1 {
+        end_steps.push(steps);
+        // print_map(&map);
+        return;
+    }
+    let neighbors = get_legal_neighbors_part2(pos, map);
+    for neighbor in neighbors {
+        map[neighbor.0.x][neighbor.0.y] = VISITED;
+        walk_part2(end_steps, map, steps + neighbor.1, &neighbor.0);
+        map[neighbor.0.x][neighbor.0.y] = PATH;
+    }
+}
+fn get_legal_neighbors_part2(s: &Coordinate, map: &Vec<Vec<u8>>) -> Vec<(Coordinate, usize)> {
+    let mut neighbors = Vec::new();
+    if s.x > 0 && map[s.x - 1][s.y] != ROCK && map[s.x - 1][s.y] != VISITED {
+        if map[s.x - 1][s.y] == b'^' {
+            neighbors.push((Coordinate::new(s.x - 2, s.y), 2));
+        } else  {
+            neighbors.push((Coordinate::new(s.x - 1, s.y), 1));
+        }
+    }
+    if s.x + 1 < map.len() && map[s.x + 1][s.y] != ROCK && map[s.x + 1][s.y] != VISITED {
+        if map[s.x + 1][s.y] == b'v' {
+            neighbors.push((Coordinate::new(s.x + 2, s.y), 2));
+        } else {
+            neighbors.push((Coordinate::new(s.x + 1, s.y), 1));
+        }
+    }
+    if s.y > 0 && map[s.x][s.y - 1] != ROCK && map[s.x][s.y - 1] != VISITED {
+        if map[s.x][s.y - 1] == b'<' {
+            neighbors.push((Coordinate::new(s.x, s.y - 2), 2));
+        } else  {
+            neighbors.push((Coordinate::new(s.x, s.y - 1), 1));
+        }
+    }
+    if s.y + 1 < map[0].len() && map[s.x][s.y + 1] != ROCK && map[s.x][s.y + 1] != VISITED {
+        if map[s.x][s.y + 1] == b'>' {
+            neighbors.push((Coordinate::new(s.x, s.y + 2), 2));
+        } else {
+            neighbors.push((Coordinate::new(s.x, s.y + 1), 1));
+        }
+    }
+    neighbors
 }
 fn print_map(map: &Vec<Vec<u8>>) {
     map.iter().for_each(|v| {
