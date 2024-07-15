360dcb8f93b8d5260af12c5cddabedc025839348
nyaaar-debian <nyaaarltahotep@163.com>
2024-07-15 19:41:01 +0800

    day25another try

--- src/day25/mod.rs
+++ src/day25/mod.rs
@@ -1,14 +1,22 @@
-use core::panic;
-use std::collections::HashMap;
+use std::{
+    collections::{HashMap, HashSet},
+    fmt::Debug,
+};
 
-#[derive(Clone, PartialEq, PartialOrd, Debug)]
+#[derive(Clone, PartialEq, PartialOrd)]
 struct Point {
     name: String,
     to: Vec<String>,
 }
 
+impl Debug for Point {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        f.debug_struct("Point").field("name", &self.name).finish()
+    }
+}
+
 pub fn part01(input: &str) -> Option<i64> {
-    let mut points_map: HashMap<&str, Point> = input
+    let points: Vec<Point> = input
         .lines()
         .map(|line| {
             let (l, r) = line.split_once(": ").unwrap();
@@ -17,10 +25,9 @@ pub fn part01(input: &str) -> Option<i64> {
                 name: l.to_string(),
                 to,
             };
-            return (l, p);
+            return p;
         })
-        .collect::<HashMap<&str, Point>>();
-    let mut points: Vec<Point> = Vec::new();
+        .collect::<Vec<Point>>();
     let p = &points[0];
     let mut l = HashMap::new();
     l.insert(p.name.as_str(), p);
@@ -34,24 +41,41 @@ fn cut<'a>(
     r: &mut HashMap<&'a str, &'a Point>,
 ) -> usize {
     if points.is_empty() {
-        let connect_lines: usize = l
-            .iter()
-            .map(|(k, v)| {
-                let t =
+        let mut connects = HashSet::new();
+        l.iter()
+            .flat_map(|(k, v)| {
+                let t:Vec<(String,String)> =
                     v.to.iter()
-                        .map(|name| {
-                            if r.contains_key(name.as_str()) {
-                                return 1;
+                        .flat_map(|name| {
+                            if let Some(r_name) = r.get(name.as_str()) {
+                                return Some((name.clone(), r_name.name.clone()));
                             }
-                            return 0;
+                            None
                         })
-                        .sum::<usize>();
+                        .collect();
                 t
             })
-            .sum();
-        print!("{}", connect_lines);
-        if connect_lines == 3 {
-            println!("{:?}\n{:?}", &l, &r);
+            .for_each(|t| {
+                connects.insert(t);
+            });
+        r.iter()
+            .flat_map(|(k, v)| {
+                let t: Vec<(String, String)> =
+                    v.to.iter()
+                        .filter_map(|name| {
+                            if let Some(l_name) = l.get(name.as_str()) {
+                                return Some((l_name.name.clone(), name.clone()));
+                            }
+                            None
+                        })
+                        .collect();
+                t
+            })
+            .for_each(|t| {
+                connects.insert(t);
+            });
+        if connects.len() == 3 {
+            println!("{:?}\n{:?}", &l.values(), &r.values());
             return l.len() * r.len();
         }
         return 0;
